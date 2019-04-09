c641dea113a68028aadcfd8da3b450124ffb095a
prsioner <731378770@qq.com>
2019-04-09 10:10:09 +0800

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+
+export default class AlignItemsBasics extends Component {
+    render() {
+        return (
+            // 尝试把`alignItems`改为`flex-start`看看
+            // 尝试把`justifyContent`改为`flex-end`看看
+            // 尝试把`flexDirection`改为`row`看看
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'stretch',
+            }}>
+                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+                <View style={{height: 50, backgroundColor: 'skyblue'}} />
+                <View style={{height: 100, backgroundColor: 'steelblue'}} />
+            </View>
+        );
+    }
+};
