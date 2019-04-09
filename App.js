4397a30c49e18d8080acaacca17f1b692411a2e7
prsioner <731378770@qq.com>
2019-04-09 20:38:09 +0800

    1.绘制了一个登录页面

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
+ * 中文地址：https://reactnative.cn/docs/layout-props/
  *
  * @format
  * @flow
  */
 
 import React, { Component } from 'react';
-import { AppRegistry, View } from 'react-native';
+import { AppRegistry,StyleSheet,Text, View,TextInput,Button ,Alert,Image,ImageBackground} from 'react-native';
 
-export default class AlignItemsBasics extends Component {
+
+
+import loginComponentBack from './img/common_icon_arrow_back.png'
+import weChatIcon from './img/common_share_logo_wechat.png'
+
+export default class UserLoginComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {account: '',password:''};
+    }
     render() {
         return (
             // 尝试把`alignItems`改为`flex-start`看看
@@ -18,13 +28,80 @@ export default class AlignItemsBasics extends Component {
             <View style={{
                 flex: 1,
                 flexDirection: 'column',
-                justifyContent: 'center',
+                //justifyContent: 'center',
                 alignItems: 'stretch',
             }}>
-                <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
-                <View style={{height: 50, backgroundColor: 'skyblue'}} />
-                <View style={{height: 100, backgroundColor: 'steelblue'}} />
+                {/*返回键*/}
+                <View >
+
+                    <Image  source={loginComponentBack} style={styles.arrowback}/>
+
+                </View>
+
+                {/*登录和输入框---alignItems决定了子元素在次轴方向的排列方式（此样式设置在父元素上）*/}
+                <View style={{marginTop:100,alignItems:'center'}}>
+
+                    <Text style={styles.loginTextStyle}>登录</Text>
+                    <TextInput
+                        style={{height: 40}}
+                        placeholder="请输入账号"
+                        onChangeText={(account) => this.setState({account})}
+                    />
+                    <TextInput
+                        style={{height: 40}}
+                        placeholder="请输入密码"
+                        onChangeText={(password) => this.setState({password})}
+                    />
+
+                </View>
+
+
+                    <Button
+                        style={{marginTop:20,height: 70,paddingLeft:20,paddingRight:20,textColor:'write'}}
+                        onPress={() => {
+                            Alert.alert((this.state.account==null||this.state.account=="" ||
+                                this.state.password == null || this.state.password=="")? "请输入账号或者密码":"账号:"+this.state.account+'\n'+"密码："+this.state.password);
+                        }}
+                        title="登录"
+                    />
+                {/*第三方登录*/}
+                <View style={{marginTop:20,flexDirection:'row',justifyContent:'space-between'}} >
+                    <Text style={styles.registerAccount}>注册账号</Text>
+                    <Text style={styles.registerAccount}>忘记密码</Text>
+
+                </View>
+
+                <View style={{marginTop:60,flexDirection:'column',alignItems:'center'}} >
+                    <Text style={styles.registerAccount}>第三方登录</Text>
+
+                    <Image source={weChatIcon} style={styles.weChatIconStyle}/>
+                </View>
+
+
             </View>
         );
     }
 };
+
+const styles  = StyleSheet.create({
+    arrowback:{
+        width:50,
+        height:50
+    },
+    loginTextStyle:{
+        color:'black',
+        fontWeight: 'bold',
+        fontSize: 30,
+
+    },
+    registerAccount:{
+        color:'blue',
+        fontSize:16,
+    },
+    weChatIconStyle:{
+        marginTop:20,
+        width:50,
+        height:50
+    }
+
+});
---
+++ development_document/git版本控制.md
@@ -0,0 +1,10 @@
+1.VCS——>import into version control——>create git repository
+
+2.选中工程文件夹 右键——>git ——> add
+
+2.github上新建名为FirstReactProject 的仓库
+
+3.工程主路径下git bush here 打开终端依次输入：
+        git commit -m "first commit"
+        git remote add origin git@github.com:prsioner/FirstReactProject.git
+        git push -u origin master
